8ad9b127a54c1c32107ddf6d92f501271f72df22
Charles Frankston <cbf@mit.edu>
2026-01-01 17:50:19 -0500

    Collection of CBF tools

---
+++ SignPowershellScript.ps1
@@ -0,0 +1,236 @@
+
+# To run this for the first time, you'll need to manually:
+# Set-ExecutionPolicy ByPass
+
+param(
+    [Parameter(Mandatory)] [String]$fileToSign
+)
+
+$authenticode = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -eq "CN=ATA Authenticode"}
+
+#Write-Host "a." + $authenticode.GetExpirationDateString()
+
+# Clear out any old Authenticode certs
+
+if ($authenticode) {
+    $most_recent = ($authenticode.GetExpirationDateString() | Measure-Object -Maximum).Maximum
+    foreach ($a in $authenticode)
+    {
+        if ($authenticode.GetExpirationDateString() -ne $most_recent)
+        {
+            Remove-Item -Path ("Cert:\LocalMachine\My\" + $a.Thumbprint)
+        }
+    }
+}
+
+# Try again to make sure we get just one cert
+$authenticode = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -eq "CN=ATA Authenticode"}
+
+#Write-Host "b." + $authenticode.GetExpirationDateString()
+
+# Get a new Signing cert if we need to
+if (-not $authenticode -or [datetime]$authenticode.GetExpirationDateString() -lt (Get-Date) )
+{  
+    # Generate a self-signed Authenticode certificate in the local computer's personal certificate store.
+    $authenticode = New-SelfSignedCertificate -Subject "ATA Authenticode" -CertStoreLocation Cert:\LocalMachine\My -Type CodeSigningCert
+
+    # Write-Host "c." + $authenticode.GetExpirationDateString()
+
+    # Add the self-signed Authenticode certificate to the computer's root certificate store.
+    ## Create an object to represent the LocalMachine\Root certificate store.
+    $rootStore = [System.Security.Cryptography.X509Certificates.X509Store]::new("Root","LocalMachine")
+    ## Open the root certificate store for reading and writing.
+    $rootStore.Open("ReadWrite")
+    ## Add the certificate stored in the $authenticode variable.
+    $rootStore.Add($authenticode)
+    ## Close the root certificate store.
+    $rootStore.Close()
+
+}
+
+if (-not (Get-ChildItem Cert:\LocalMachine\TrustedPublisher | Where-Object {$_.Subject -eq "CN=ATA Authenticode"}))
+{
+    # Add the self-signed Authenticode certificate to the computer's trusted publishers certificate store.
+    ## Create an object to represent the LocalMachine\TrustedPublisher certificate store.
+    $publisherStore = [System.Security.Cryptography.X509Certificates.X509Store]::new("TrustedPublisher","LocalMachine")
+    ## Open the TrustedPublisher certificate store for reading and writing.
+    $publisherStore.Open("ReadWrite")
+    ## Add the certificate stored in the $authenticode variable.
+    $publisherStore.Add($authenticode)
+    ## Close the TrustedPublisher certificate store.
+    $publisherStore.Close()
+}
+
+# printouts for debugging
+# # Confirm if the self-signed Authenticode certificate exists in the computer's Personal certificate store
+#  Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -eq "CN=ATA Authenticode"}
+# # Confirm if the self-signed Authenticode certificate exists in the computer's Root certificate store
+#  Get-ChildItem Cert:\LocalMachine\Root | Where-Object {$_.Subject -eq "CN=ATA Authenticode"}
+# # Confirm if the self-signed Authenticode certificate exists in the computer's Trusted Publishers certificate store
+#  Get-ChildItem Cert:\LocalMachine\TrustedPublisher | Where-Object {$_.Subject -eq "CN=ATA Authenticode"}
+
+# Get the code-signing certificate from the local computer's certificate store with the name *ATA Authenticode* and store it to the $codeCertificate variable.
+$codeCertificate = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Subject -eq "CN=ATA Authenticode"}
+
+# PARAMETERS:
+# FilePath - Specifies the file path of the PowerShell script to sign, eg. C:\ATA\myscript.ps1.
+# Certificate - Specifies the certificate to use when signing the script.
+# TimeStampServer - Specifies the trusted timestamp server that adds a timestamp to your script's digital signature. Adding a timestamp ensures that your code will not expire when the signing certificate expires.
+Set-AuthenticodeSignature -FilePath $fileToSign -Certificate $codeCertificate -TimeStampServer http://timestamp.digicert.com
+
+# Restore more secure execution policy
+# Set-ExecutionPolicy AllSigned
+
+
+
+# SIG # Begin signature block
+# MIIb1AYJKoZIhvcNAQcCoIIbxTCCG8ECAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUTTVKkcOiZN1//dtwYu30oXlZ
+# WmigghZEMIIDBjCCAe6gAwIBAgIQfgB16oPhJYdCA8+jPApo0zANBgkqhkiG9w0B
+# AQsFADAbMRkwFwYDVQQDDBBBVEEgQXV0aGVudGljb2RlMB4XDTI1MTIyNjAxMDQy
+# NloXDTI2MTIyNjAxMjQyNlowGzEZMBcGA1UEAwwQQVRBIEF1dGhlbnRpY29kZTCC
+# ASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMMrCYmt0EVuVL2wCv33ZPGX
+# nrh/86CD58QJNSQauGsvc8YkiCTyCdEtI12E19cfzSX5tP9XpQ2/MQo3/uoL2opi
+# pn8asto0QaOqeRBwR9QFkOU4hAQiSZQYIoFkYP4b3cATmD84MNqpeti8Aq779u6S
+# FG4SrQBIfa0AUXsGBJoVik7OlOthD8mYgRIkRtNq0o+Ag9pQqSX26GjGj9p5RLZD
+# h6k+wqNjv+PcZ4ZqRhrwkZ2jCqNLNVeXtXKv7iuFjnOLBSeCWCbUzr8S6q/y1bqi
+# Oam6lmdOBUn9WCI6vo2GgM7zZ39n0o+h1ofCQqCaV5WdCsb9zxysyzShlhPD02UC
+# AwEAAaNGMEQwDgYDVR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMB0G
+# A1UdDgQWBBQpLfRszBGTOzgOjKEXLkqGA85IkzANBgkqhkiG9w0BAQsFAAOCAQEA
+# eEHhLl9579/ooulo9zXT4+3HUdqsQ9fmNQOpCvCCGLuhsZVWiSE6U9T3eJbaJcD7
+# ItCKQpLh4x5dskwjmToMJ2DKL04qIwPmDvqKG9sy07qqOqCjHfpw7JNSMxyEwZlT
+# k5eZ0Az6UvYvggAxGX7hVaW3S/3v0vfueGd1NHWKm1RDrT4Fc+20YFw4Avu3Tofx
+# KtKowmLQNwnpHWbrXTEOi8Bnn4Et2likNP/iB/Nu06XTp6zQ3W4dlnrjQDdrOMNt
+# 3y4FBa9iOt+xL6IQt7r92zFOOP2JZ/BdneBssB229npwtOyaCSJZ2F+jN7YgIqn8
+# G30CeXyntqIodLCjoJFOsjCCBY0wggR1oAMCAQICEA6bGI750C3n79tQ4ghAGFow
+# DQYJKoZIhvcNAQEMBQAwZTELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0
+# IEluYzEZMBcGA1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTEkMCIGA1UEAxMbRGlnaUNl
+# cnQgQXNzdXJlZCBJRCBSb290IENBMB4XDTIyMDgwMTAwMDAwMFoXDTMxMTEwOTIz
+# NTk1OVowYjELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcG
+# A1UECxMQd3d3LmRpZ2ljZXJ0LmNvbTEhMB8GA1UEAxMYRGlnaUNlcnQgVHJ1c3Rl
+# ZCBSb290IEc0MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAv+aQc2je
+# u+RdSjwwIjBpM+zCpyUuySE98orYWcLhKac9WKt2ms2uexuEDcQwH/MbpDgW61bG
+# l20dq7J58soR0uRf1gU8Ug9SH8aeFaV+vp+pVxZZVXKvaJNwwrK6dZlqczKU0RBE
+# EC7fgvMHhOZ0O21x4i0MG+4g1ckgHWMpLc7sXk7Ik/ghYZs06wXGXuxbGrzryc/N
+# rDRAX7F6Zu53yEioZldXn1RYjgwrt0+nMNlW7sp7XeOtyU9e5TXnMcvak17cjo+A
+# 2raRmECQecN4x7axxLVqGDgDEI3Y1DekLgV9iPWCPhCRcKtVgkEy19sEcypukQF8
+# IUzUvK4bA3VdeGbZOjFEmjNAvwjXWkmkwuapoGfdpCe8oU85tRFYF/ckXEaPZPfB
+# aYh2mHY9WV1CdoeJl2l6SPDgohIbZpp0yt5LHucOY67m1O+SkjqePdwA5EUlibaa
+# RBkrfsCUtNJhbesz2cXfSwQAzH0clcOP9yGyshG3u3/y1YxwLEFgqrFjGESVGnZi
+# fvaAsPvoZKYz0YkH4b235kOkGLimdwHhD5QMIR2yVCkliWzlDlJRR3S+Jqy2QXXe
+# eqxfjT/JvNNBERJb5RBQ6zHFynIWIgnffEx1P2PsIV/EIFFrb7GrhotPwtZFX50g
+# /KEexcCPorF+CiaZ9eRpL5gdLfXZqbId5RsCAwEAAaOCATowggE2MA8GA1UdEwEB
+# /wQFMAMBAf8wHQYDVR0OBBYEFOzX44LScV1kTN8uZz/nupiuHA9PMB8GA1UdIwQY
+# MBaAFEXroq/0ksuCMS1Ri6enIZ3zbcgPMA4GA1UdDwEB/wQEAwIBhjB5BggrBgEF
+# BQcBAQRtMGswJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBD
+# BggrBgEFBQcwAoY3aHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0
+# QXNzdXJlZElEUm9vdENBLmNydDBFBgNVHR8EPjA8MDqgOKA2hjRodHRwOi8vY3Js
+# My5kaWdpY2VydC5jb20vRGlnaUNlcnRBc3N1cmVkSURSb290Q0EuY3JsMBEGA1Ud
+# IAQKMAgwBgYEVR0gADANBgkqhkiG9w0BAQwFAAOCAQEAcKC/Q1xV5zhfoKN0Gz22
+# Ftf3v1cHvZqsoYcs7IVeqRq7IviHGmlUIu2kiHdtvRoU9BNKei8ttzjv9P+Aufih
+# 9/Jy3iS8UgPITtAq3votVs/59PesMHqai7Je1M/RQ0SbQyHrlnKhSLSZy51PpwYD
+# E3cnRNTnf+hZqPC/Lwum6fI0POz3A8eHqNJMQBk1RmppVLC4oVaO7KTVPeix3P0c
+# 2PR3WlxUjG/voVA9/HYJaISfb8rbII01YBwCA8sgsKxYoA5AY8WYIsGyWfVVa88n
+# q2x2zm8jLfR+cWojayL/ErhULSd+2DrZ8LaHlv1b0VysGMNNn3O3AamfV6peKOK5
+# lDCCBrQwggScoAMCAQICEA3HrFcF/yGZLkBDIgw6SYYwDQYJKoZIhvcNAQELBQAw
+# YjELMAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQ
+# d3d3LmRpZ2ljZXJ0LmNvbTEhMB8GA1UEAxMYRGlnaUNlcnQgVHJ1c3RlZCBSb290
+# IEc0MB4XDTI1MDUwNzAwMDAwMFoXDTM4MDExNDIzNTk1OVowaTELMAkGA1UEBhMC
+# VVMxFzAVBgNVBAoTDkRpZ2lDZXJ0LCBJbmMuMUEwPwYDVQQDEzhEaWdpQ2VydCBU
+# cnVzdGVkIEc0IFRpbWVTdGFtcGluZyBSU0E0MDk2IFNIQTI1NiAyMDI1IENBMTCC
+# AiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBALR4MdMKmEFyvjxGwBysdduj
+# Rmh0tFEXnU2tjQ2UtZmWgyxU7UNqEY81FzJsQqr5G7A6c+Gh/qm8Xi4aPCOo2N8S
+# 9SLrC6Kbltqn7SWCWgzbNfiR+2fkHUiljNOqnIVD/gG3SYDEAd4dg2dDGpeZGKe+
+# 42DFUF0mR/vtLa4+gKPsYfwEu7EEbkC9+0F2w4QJLVSTEG8yAR2CQWIM1iI5PHg6
+# 2IVwxKSpO0XaF9DPfNBKS7Zazch8NF5vp7eaZ2CVNxpqumzTCNSOxm+SAWSuIr21
+# Qomb+zzQWKhxKTVVgtmUPAW35xUUFREmDrMxSNlr/NsJyUXzdtFUUt4aS4CEeIY8
+# y9IaaGBpPNXKFifinT7zL2gdFpBP9qh8SdLnEut/GcalNeJQ55IuwnKCgs+nrpuQ
+# NfVmUB5KlCX3ZA4x5HHKS+rqBvKWxdCyQEEGcbLe1b8Aw4wJkhU1JrPsFfxW1gao
+# u30yZ46t4Y9F20HHfIY4/6vHespYMQmUiote8ladjS/nJ0+k6MvqzfpzPDOy5y6g
+# qztiT96Fv/9bH7mQyogxG9QEPHrPV6/7umw052AkyiLA6tQbZl1KhBtTasySkuJD
+# psZGKdlsjg4u70EwgWbVRSX1Wd4+zoFpp4Ra+MlKM2baoD6x0VR4RjSpWM8o5a6D
+# 8bpfm4CLKczsG7ZrIGNTAgMBAAGjggFdMIIBWTASBgNVHRMBAf8ECDAGAQH/AgEA
+# MB0GA1UdDgQWBBTvb1NK6eQGfHrK4pBW9i/USezLTjAfBgNVHSMEGDAWgBTs1+OC
+# 0nFdZEzfLmc/57qYrhwPTzAOBgNVHQ8BAf8EBAMCAYYwEwYDVR0lBAwwCgYIKwYB
+# BQUHAwgwdwYIKwYBBQUHAQEEazBpMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5k
+# aWdpY2VydC5jb20wQQYIKwYBBQUHMAKGNWh0dHA6Ly9jYWNlcnRzLmRpZ2ljZXJ0
+# LmNvbS9EaWdpQ2VydFRydXN0ZWRSb290RzQuY3J0MEMGA1UdHwQ8MDowOKA2oDSG
+# Mmh0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydFRydXN0ZWRSb290RzQu
+# Y3JsMCAGA1UdIAQZMBcwCAYGZ4EMAQQCMAsGCWCGSAGG/WwHATANBgkqhkiG9w0B
+# AQsFAAOCAgEAF877FoAc/gc9EXZxML2+C8i1NKZ/zdCHxYgaMH9Pw5tcBnPw6O6F
+# TGNpoV2V4wzSUGvI9NAzaoQk97frPBtIj+ZLzdp+yXdhOP4hCFATuNT+ReOPK0mC
+# efSG+tXqGpYZ3essBS3q8nL2UwM+NMvEuBd/2vmdYxDCvwzJv2sRUoKEfJ+nN57m
+# QfQXwcAEGCvRR2qKtntujB71WPYAgwPyWLKu6RnaID/B0ba2H3LUiwDRAXx1Neq9
+# ydOal95CHfmTnM4I+ZI2rVQfjXQA1WSjjf4J2a7jLzWGNqNX+DF0SQzHU0pTi4dB
+# wp9nEC8EAqoxW6q17r0z0noDjs6+BFo+z7bKSBwZXTRNivYuve3L2oiKNqetRHdq
+# fMTCW/NmKLJ9M+MtucVGyOxiDf06VXxyKkOirv6o02OoXN4bFzK0vlNMsvhlqgF2
+# puE6FndlENSmE+9JGYxOGLS/D284NHNboDGcmWXfwXRy4kbu4QFhOm0xJuF2EZAO
+# k5eCkhSxZON3rGlHqhpB/8MluDezooIs8CVnrpHMiD2wL40mm53+/j7tFaxYKIqL
+# 0Q4ssd8xHZnIn/7GELH3IdvG2XlM9q7WP/UwgOkw/HQtyRN62JK4S1C8uw3PdBun
+# vAZapsiI5YKdvlarEvf8EA+8hcpSM9LHJmyrxaFtoza2zNaQ9k+5t1wwggbtMIIE
+# 1aADAgECAhAKgO8YS43xBYLRxHanlXRoMA0GCSqGSIb3DQEBCwUAMGkxCzAJBgNV
+# BAYTAlVTMRcwFQYDVQQKEw5EaWdpQ2VydCwgSW5jLjFBMD8GA1UEAxM4RGlnaUNl
+# cnQgVHJ1c3RlZCBHNCBUaW1lU3RhbXBpbmcgUlNBNDA5NiBTSEEyNTYgMjAyNSBD
+# QTEwHhcNMjUwNjA0MDAwMDAwWhcNMzYwOTAzMjM1OTU5WjBjMQswCQYDVQQGEwJV
+# UzEXMBUGA1UEChMORGlnaUNlcnQsIEluYy4xOzA5BgNVBAMTMkRpZ2lDZXJ0IFNI
+# QTI1NiBSU0E0MDk2IFRpbWVzdGFtcCBSZXNwb25kZXIgMjAyNSAxMIICIjANBgkq
+# hkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA0EasLRLGntDqrmBWsytXum9R/4ZwCgHf
+# yjfMGUIwYzKomd8U1nH7C8Dr0cVMF3BsfAFI54um8+dnxk36+jx0Tb+k+87H9WPx
+# NyFPJIDZHhAqlUPt281mHrBbZHqRK71Em3/hCGC5KyyneqiZ7syvFXJ9A72wzHpk
+# BaMUNg7MOLxI6E9RaUueHTQKWXymOtRwJXcrcTTPPT2V1D/+cFllESviH8YjoPFv
+# ZSjKs3SKO1QNUdFd2adw44wDcKgH+JRJE5Qg0NP3yiSyi5MxgU6cehGHr7zou1zn
+# OM8odbkqoK+lJ25LCHBSai25CFyD23DZgPfDrJJJK77epTwMP6eKA0kWa3osAe8f
+# cpK40uhktzUd/Yk0xUvhDU6lvJukx7jphx40DQt82yepyekl4i0r8OEps/FNO4ah
+# fvAk12hE5FVs9HVVWcO5J4dVmVzix4A77p3awLbr89A90/nWGjXMGn7FQhmSlIUD
+# y9Z2hSgctaepZTd0ILIUbWuhKuAeNIeWrzHKYueMJtItnj2Q+aTyLLKLM0MheP/9
+# w6CtjuuVHJOVoIJ/DtpJRE7Ce7vMRHoRon4CWIvuiNN1Lk9Y+xZ66lazs2kKFSTn
+# nkrT3pXWETTJkhd76CIDBbTRofOsNyEhzZtCGmnQigpFHti58CSmvEyJcAlDVcKa
+# cJ+A9/z7eacCAwEAAaOCAZUwggGRMAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFOQ7
+# /PIx7f391/ORcWMZUEPPYYzoMB8GA1UdIwQYMBaAFO9vU0rp5AZ8esrikFb2L9RJ
+# 7MtOMA4GA1UdDwEB/wQEAwIHgDAWBgNVHSUBAf8EDDAKBggrBgEFBQcDCDCBlQYI
+# KwYBBQUHAQEEgYgwgYUwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0
+# LmNvbTBdBggrBgEFBQcwAoZRaHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL0Rp
+# Z2lDZXJ0VHJ1c3RlZEc0VGltZVN0YW1waW5nUlNBNDA5NlNIQTI1NjIwMjVDQTEu
+# Y3J0MF8GA1UdHwRYMFYwVKBSoFCGTmh0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9E
+# aWdpQ2VydFRydXN0ZWRHNFRpbWVTdGFtcGluZ1JTQTQwOTZTSEEyNTYyMDI1Q0Ex
+# LmNybDAgBgNVHSAEGTAXMAgGBmeBDAEEAjALBglghkgBhv1sBwEwDQYJKoZIhvcN
+# AQELBQADggIBAGUqrfEcJwS5rmBB7NEIRJ5jQHIh+OT2Ik/bNYulCrVvhREafBYF
+# 0RkP2AGr181o2YWPoSHz9iZEN/FPsLSTwVQWo2H62yGBvg7ouCODwrx6ULj6hYKq
+# dT8wv2UV+Kbz/3ImZlJ7YXwBD9R0oU62PtgxOao872bOySCILdBghQ/ZLcdC8cbU
+# UO75ZSpbh1oipOhcUT8lD8QAGB9lctZTTOJM3pHfKBAEcxQFoHlt2s9sXoxFizTe
+# HihsQyfFg5fxUFEp7W42fNBVN4ueLaceRf9Cq9ec1v5iQMWTFQa0xNqItH3CPFTG
+# 7aEQJmmrJTV3Qhtfparz+BW60OiMEgV5GWoBy4RVPRwqxv7Mk0Sy4QHs7v9y69NB
+# qycz0BZwhB9WOfOu/CIJnzkQTwtSSpGGhLdjnQ4eBpjtP+XB3pQCtv4E5UCSDag6
+# +iX8MmB10nfldPF9SVD7weCC3yXZi/uuhqdwkgVxuiMFzGVFwYbQsiGnoa9F5AaA
+# yBjFBtXVLcKtapnMG3VH3EmAp/jsJ3FVF3+d1SVDTmjFjLbNFZUWMXuZyvgLfgyP
+# ehwJVxwC+UpX2MSey2ueIu9THFVkT+um1vshETaWyQo8gmBto/m3acaP9QsuLj3F
+# NwFlTxq25+T4QwX9xa6ILs84ZPvmpovq90K8eWyG2N01c4IhSOxqt81nMYIE+jCC
+# BPYCAQEwLzAbMRkwFwYDVQQDDBBBVEEgQXV0aGVudGljb2RlAhB+AHXqg+Elh0ID
+# z6M8CmjTMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3AgEMMQowCKACgAChAoAAMBkG
+# CSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisGAQQBgjcCAQsxDjAMBgorBgEE
+# AYI3AgEVMCMGCSqGSIb3DQEJBDEWBBRzhI09LX7cECbHNJy/RP/wwbQ/QzANBgkq
+# hkiG9w0BAQEFAASCAQCxxxJGyH9xAHjsDFw5rsDBG49mhW+ZCnkml7pAHX+A1rPX
+# TNSLXtorUYmXqkMXSeGk2p2/8RktnFzuKjnJgVamodSuea3FPMgRu6Yq6P0/Bx/D
+# 7ZBc8SQvg6KYsdLCl71Ri+JPD9k3HRJau6qkXL6DPZb6oQDSl8+kB4oD6qVmkkkT
+# sUREv+cBmXI/Qov0ItHmrtJ6XqsiiS+AwYGbWTy1SMMZ0J72MOUKhIrLnXTefXEi
+# tDih6bL8tJRqLo4K0EEGncRKTrlCZ1WJC67ZfTp9sGtVuBPlw5TpG3dxL1arG5Gb
+# z/tk7podJik+oHAnKN8Y9G5mgvw+z0Fx2LGKVy+7oYIDJjCCAyIGCSqGSIb3DQEJ
+# BjGCAxMwggMPAgEBMH0waTELMAkGA1UEBhMCVVMxFzAVBgNVBAoTDkRpZ2lDZXJ0
+# LCBJbmMuMUEwPwYDVQQDEzhEaWdpQ2VydCBUcnVzdGVkIEc0IFRpbWVTdGFtcGlu
+# ZyBSU0E0MDk2IFNIQTI1NiAyMDI1IENBMQIQCoDvGEuN8QWC0cR2p5V0aDANBglg
+# hkgBZQMEAgEFAKBpMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcN
+# AQkFMQ8XDTI2MDEwMTIyNDgyOVowLwYJKoZIhvcNAQkEMSIEIOj4vm+FYgQRQDSc
+# GZ9AK0XdqsiNleII1aDjD/1mnYzjMA0GCSqGSIb3DQEBAQUABIICABovGSEH4odn
+# S7hyaDl82/RCn2qZkLBNnS/M4Su1yWVMBI/gS2W4jwHFTYMOyhe0HvU4n+36++fO
+# u2aVO97aygEccZzNJ6p6uwIifCh0TBm8RDq1rQHpBJmgVeGOsfyq7OELoDmFAGXt
+# LkYIb/CY9UDAl0a+uX4vrYUKiufUmys1OsAS+EbUwD9jaCkAi8OEQ/PTYF6uB4aa
+# j+1FxAmvbyurNgPlVg6ASgGpX4svqZHx4RQ1/fi95BPQa4gDXx+M9XYbmzd92unj
+# 8OYNfNSF+unp0KLT8iEo9avbU3FZuiz8acSCp/oliqBYwGWvsLUSDn3VJ4jF36JK
+# RoC3aJXdgBEGeikbr8aPh2zZdbahl4GHcf4oWZU++Myt7AF0iMh2MBqhAm2t3bWl
+# 7s2YlYkssyPJILTRH9mG1Ofu0Ti8s9QFSSi+EWN+UsQLnyitW1CZbNkGe5U8+SJ/
+# YaWPSb4KGdtxHVOHpRh+HDMfy58Y/qvXllDS9mcbmEWEHu3ICoOS9SFjoUTT3zLk
+# zQlvRdyKWiHCJPn7+F9S15OMnwE35aKpqJqHtn3AxxnwYHUAIGva+eXKTYzBw/eM
+# VRoWo0Lr7fiVvyc5Hke5pUewSM8s/OTQR1nkA/AeW+6E52cvNCctvIQ+5djIPXdU
+# 3M/IdyQi1OtdslpFUWgmJtxnHXVGzCpz
+# SIG # End signature block
